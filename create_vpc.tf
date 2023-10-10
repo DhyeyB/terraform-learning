5397083699ca41ff2d06cab71c3b71a9441ac3a3
DhyeyB <dhyeybhattasana1999@gmail.com>
2023-10-10 17:41:34 +0530

    nginx installation using ansible and terraform

--- create_vpc.tf
+++ create_vpc.tf
@@ -18,37 +18,55 @@ resource "aws_security_group" "public-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_instance" "punlic-instance" {
-  ami           = "ami-08e5424edfe926b43"
-  instance_type = "t2.micro"
-  # subnet_id       = "$(vpc.aws_subnet.public_subnet.id)"
+resource "aws_instance" "nginx" {
+  ami                         = "ami-08e5424edfe926b43"
+  instance_type               = "t2.micro"
   subnet_id                   = module.vpc.public_subnet_ids["Public-subnet-1"]
   security_groups             = [aws_security_group.public-sg.id]
   key_name                    = "public-instance"
   associate_public_ip_address = true
+
+  provisioner "remote-exec" {
+    inline = ["echo 'wait untill ssh is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/Downloads/public-instance.pem")
+      host        = aws_instance.nginx.public_ip
+    }
+  }
+
+  # provisioner "local-exec" {
+  #   command = "ansible-playbook -i ${aws_instance.nginx.public_ip}, --private-key ${"~/Downloads/public-instance.pem"} ./ansible/nginx.yml"
+  # }
 }
 
 
-###############     Private EC2 Instance      ###########
-resource "aws_security_group" "private-sg" {
-  name_prefix = "private-sg"
-  vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.public-sg.id]
+resource "null_resource" "run_ansible" {
+  triggers = {
+    instance_id = aws_instance.nginx.id
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ${aws_instance.nginx.public_ip}, --private-key ~/Downloads/public-instance.pem ./ansible/nginx.yml"
   }
 }
 
-resource "aws_instance" "private-instance" {
-  ami                         = "ami-08e5424edfe926b43"
-  instance_type               = "t2.micro"
-  subnet_id                   = module.vpc.private_subnet_ids["Private-subnet-1"]
-  security_groups             = [aws_security_group.private-sg.id]
-  key_name                    = "private-instance"
-  associate_public_ip_address = true
-}
